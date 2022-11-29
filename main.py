eedcb786b151c9a231363c88cdc4b52e127c3d2e
paulshuba <apshuba@gmail.com>
2022-11-29 21:25:45 +0300

    add print numbers

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text1 = input('input first word: ')
-text2 = input('input second word: ')
-text3 = input('input third word: ')
-print(text3, text2, text1)
+number = input('input your number: ')
+number1 = number[0]
+number2 = number[1]
+number3 = number[2]
+print(number1)
+print(number2)
+print(number3)
+number1 = int(number1)
+number2 = int(number2)
+number3 = int(number3)
+print(number1 + number2 + number3)
