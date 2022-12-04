97f5d76d97c9687c9e9b25f22089de06c0cf6dc6
paulshuba <apshuba@gmail.com>
2022-12-04 14:33:42 +0300

    edit task_3

--- lesson_4/task_3.py
+++ lesson_4/task_3.py
@@ -1,6 +1,7 @@
+# Create dictionary with keys from 0 to N, and values as dictionaries with keys 'name' and 'e-mail'
+# and values, entered from keyboard.
 
-n = input('input n: ')
+n = input('Enter N: ')
 n = int(n)
-
-dict2 = {i:{'name': input('input name: '), 'e-mail': input('input e-mail:')} for i in range((n+1))}
-print(dict2)
+dict1 = {i:{'name': input('Enter name: '), 'e-mail': input('Enter e-mail:')} for i in range((n+1))}
+print(dict1)
