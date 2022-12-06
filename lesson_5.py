0a75f92296ed7a04bb73372c578eb9c0666e7f6f
paulshuba <apshuba@gmail.com>
2022-12-06 20:42:43 +0300

    edit files

--- lesson_5.py
+++ lesson_5.py
@@ -3,20 +3,14 @@
 #     print('this')
 # else:
 # #     print('that')
-# a = input()
-# for b in a:
-#     a.isalpha()
-#     print(b)
-#
 
 # numbers = [2, 3, 4, 5, 6, 2]
 # while 2 in numbers:
 #     numbers.remove(2)
 #     print(numbers)
 
-
-text = input('input number: ')
-while not text.isdigit():
-    text = input('input number: ')
-print('congratulations, your number is: ', text)
+# text = input('input number: ')
+# while not text.isdigit():
+#     text = input('input number: ')
+# print('congratulations, your number is: ', text)
 
