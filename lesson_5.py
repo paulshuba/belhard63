4b5c1d9cded0d06af27dd9befe2a0d8ffa8ff9e7
paulshuba <apshuba@gmail.com>
2022-12-06 20:32:14 +0300

    edit files

---
+++ lesson_5.py
@@ -0,0 +1,22 @@
+# a = 4
+# if a % 2:
+#     print('this')
+# else:
+# #     print('that')
+# a = input()
+# for b in a:
+#     a.isalpha()
+#     print(b)
+#
+
+# numbers = [2, 3, 4, 5, 6, 2]
+# while 2 in numbers:
+#     numbers.remove(2)
+#     print(numbers)
+
+
+text = input('input number: ')
+while not text.isdigit():
+    text = input('input number: ')
+print('congratulations, your number is: ', text)
+
