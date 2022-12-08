4e9911c5270b482c3f95bb13bab569287e404b82
paulshuba <apshuba@gmail.com>
2022-12-08 23:24:54 +0300

    edit files

--- lesson_5.py
+++ lesson_5.py
@@ -18,18 +18,44 @@
 # sum = 0
 # for i in number:
 #     sum += int(i)
-# print("sum = ", sum)
+# # print("sum = ", sum)
+#
 
-a = int(input())
-oper = input()
-b = int(input())
-if oper == '+':
-    print(a+b)
-elif oper == '*':
-    print(a*b)
-elif oper == '-':
-    print(a - b)
-elif oper == '/':
-    print(a / b)
-else:
-    print('enter valid info')
+
+# a = int(input())
+# oper = input()
+# b = int(input())
+# if oper == '+':
+#     print(a+b)
+# elif oper == '*':
+#     print(a*b)
+# elif oper == '-':
+#     print(a - b)
+# elif oper == '/':
+#     print(a / b)
+# else:
+#     print('enter valid info')
+
+# n = int(input('введите необходимое количество первых чисел n: '))
+# m = int(input('введите кратность m: '))
+# k = int(input('введите нижний предел k: '))
+#
+# for i in range (k, (k + m * (n + 1))):
+# #     if i % m == 0 and i != k:
+# # #         print(i, end=' ')
+#
+# n = int(input('введите необходимое количество первых чисел n: '))
+# m = int(input('введите кратность m: '))
+# k = int(input('введите нижний предел k: '))
+# while i > k:
+#     if i % m == 0:
+#         print(i)
+#
+# while i % m == 0:
+#     if i > k:
+#         print(i, end=' ')
+
+n = int(input('input: '))
+for i in range(2, (n+1)):
+    if i % 2 == 0:
+        print(i, end=' ')
